7f834a856712ee20ca88a901d560641a7d1d5a96
raremiroir <miro.polfliet@student.kdg.be>
2023-08-26 02:23:15 +0200

    First commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+   import Logo from "$lib/assets/mistmedia-logo.svg"
+</script>
+
+<main class="bg-[#141617] w-screen h-screen">
+  <div class="mx-auto clearance-tape w-full h-full flex items-center justify-center">
+    <div class="bg-[#141617] flex items-center justify-center flex-col gap-6 px-16 py-32">
+      <img src={Logo} alt="Mist Media Logo" width="300"  /> 
+      <h1 class="text-5xl font-bold text-center text-slate-100 m-0 p-0">
+         THIS WEBSITE IS UNDER CONSTRUCTION OR BEING MAINTAINED
+      </h1>
+      <span class="text-center italic text-slate-300 w-full m-0 p-0">
+         Please revisit another time soon. Thank you for your patience.
+      </span>
+      <div class="flex flex-col items-center justify-center gap-0.5">
+         <a href="mailto:miro@mistmedia.be" title="Send a mail to Mist Media" aria-label="Contact Mist Media" tabindex="0" 
+            class="flex items-center justify-center px-8 py-4 bg-slate-100 text-slate-950 text-lg font-semibold tracking-wide uppercase rounded-md hover:bg-[#f2a417] transition-all duration-200 ease-in">
+            Contact us
+         </a>
+         <span class="text-slate-300 italic text-sm">miro@mistmedia.be</span>
+      </div>
+    </div>
+  </div>
+</main>
